5635c1e3ac9fd295ddd0326948ba6809e37a5549
Gerald266 <58418353+Gerald266@users.noreply.github.com>
2019-12-03 19:53:33 +0800

    Initial Commit

---
+++ Controller/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using BanilaIP.WebSite.Models;
+using BanilaIP.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BanilaIP.WebSite.Controller
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        //[HttpPatch] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+           [FromQuery] string ProductId,
+           [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+
+            return Ok();
+        }
+
+
+    }
+}
